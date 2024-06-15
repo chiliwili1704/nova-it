9ddec8a8e5e3ba46975b51895986ad42d540643e
Pablo Reyes <p.reyes1704@gmail.com>
2024-06-15 12:07:28 -0600

    declaración de router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,10 @@ import {LayoutComponent} from "@nova/layout.component";
 
 export const routes: Routes = [{
    path: '', component: LayoutComponent, children: [{
-      path: '',
+      path: 'dashboard',
       loadComponent: () => import("@nova/dashboard/dashboard.component").then(value => value.DashboardComponent)
-   },]
+   },{
+      path:'ticket',
+      loadComponent:()=>import("@nova/sistemas/ticket/ticket.component").then(value => value.TicketComponent)
+   }]
 }];
