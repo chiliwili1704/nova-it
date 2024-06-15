3f9b58cf792af7608d6f59ae547b9a8b77908929
Pablo Reyes <p.reyes1704@gmail.com>
2024-06-15 03:22:16 -0600

    Declaración de módulos - Principal -Sistemas

---
+++ src/app/modulo/app.menu.ts
@@ -0,0 +1,13 @@
+export const AppMenu = [
+   {
+      label: 'Principal',
+      items: [
+         {label: 'Dashboard', icon: 'it dashboard', routerLink: ['/dashboard']}]
+   },
+   {
+      label: 'Sistemas',
+      items: [
+         {label: 'Ticket', icon: 'it onlyoffice', routerLink: ['/ticket']}, {
+            label: 'Horarios', icon: 'it schedule', routerLink: ['/horarios']
+         }]
+   }];
