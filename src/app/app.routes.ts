89d8308bca5ed84226bf3b01dcd555cc11449a24
Pablo Reyes <p.reyes1704@gmail.com>
2024-06-15 04:12:03 -0600

    Se corrige ruta comodín

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LayoutComponent} from "@nova/layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+   path: '', component: LayoutComponent, children: [{
+      path: '',
+      loadComponent: () => import("@nova/dashboard/dashboard.component").then(value => value.DashboardComponent)
+   },]
+}];
